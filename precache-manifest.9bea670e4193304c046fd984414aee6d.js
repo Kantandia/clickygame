6028e2a9b0fc44c8a74f917f8ed2b45763527192
Kantandia <taylorsdsmith@gmail.com>
2019-07-31 08:45:59 -0500

    Updates

---
+++ precache-manifest.9bea670e4193304c046fd984414aee6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d164e39f3099f1a57ade570ff34f2d6f",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "6703ccc9f35e0424b606",
+    "url": "/clickygame/static/css/main.08805bcc.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6703ccc9f35e0424b606",
+    "url": "/clickygame/static/js/main.33de20b3.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
